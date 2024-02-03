5744d06c9366e7d01e6e0379b0f5a6efb72d8d13
Thomas Croizet <46353105+SteezCram@users.noreply.github.com>
2024-02-03 18:00:03 +0900

    Fix project

--- src/PboViewer GUI/build.ps1
+++ src/PboViewer GUI/build.ps1
@@ -1,5 +1,7 @@
-$directory = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
-dotnet publish -c Release -o $directory/publish/windows --self-contained -r win-x64
-dotnet publish -c Release -o $directory/publish/macos --self-contained -r osx-x64
-dotnet publish -c Release -o $directory/publish/debian --self-contained -r debian-x64
-dotnet publish -c Release -o $directory/publish/ubuntu --self-contained -r ubuntu-x64
+$directory = [System.IO.Path]::GetDirectoryName([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition))
+
+dotnet publish -c Release -o $directory/publish/windows-x64 --self-contained -r win-x64
+dotnet publish -c Release -o $directory/publish/windows-arm64 --self-contained -r win-arm64
+dotnet publish -c Release -o $directory/publish/macos-arm64 --self-contained -r osx-arm64
+dotnet publish -c Release -o $directory/publish/linux-x64 --self-contained -r linux-x64
+dotnet publish -c Release -o $directory/publish/linux-arm64 --self-contained -r linux-arm64
