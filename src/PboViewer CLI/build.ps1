ab2f36b58d8cdb543f90fc61a86d0527acdb8e24
Thomas Croizet <46353105+SteezCram@users.noreply.github.com>
2024-07-21 19:01:27 +0200

    Update builds file

--- src/PboViewer CLI/build.ps1
+++ src/PboViewer CLI/build.ps1
@@ -1,7 +1,7 @@
 $directory = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
-dotnet publish -c Release -o $directory/publish/win-x64 --self-contained -r win-x64
-dotnet publish -c Release -o $directory/publish/win-arm64 --self-contained -r win-arm64
-dotnet publish -c Release -o $directory/publish/macos-x64 --self-contained -r osx-x64
-dotnet publish -c Release -o $directory/publish/macos-arm64 --self-contained -r osx-arm64
-dotnet publish -c Release -o $directory/publish/linux-x64 --self-contained -r linux-x64
-dotnet publish -c Release -o $directory/publish/linux-arm64 --self-contained -r linux-arm64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-windows-x64 --self-contained -r win-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-windows-arm64 --self-contained -r win-arm64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-macos-x64 --self-contained -r osx-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-macos-arm64 --self-contained -r osx-arm64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-linux-x64 --self-contained -r linux-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-linux-arm64 --self-contained -r linux-arm64
--- src/PboViewer GUI/build.ps1
+++ src/PboViewer GUI/build.ps1
@@ -1,7 +1,7 @@
 $directory = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
-dotnet publish -c Release -o $directory/publish/win-x64 --self-contained -r win-x64
-dotnet publish -c Release -o $directory/publish/win-arm64 --self-contained -r win-arm64
-dotnet publish -c Release -o $directory/publish/macos-x64 --self-contained -r osx-x64
-dotnet publish -c Release -o $directory/publish/macos-arm64 --self-contained -r osx-arm64
-dotnet publish -c Release -o $directory/publish/linux-x64 --self-contained -r linux-x64
-dotnet publish -c Release -o $directory/publish/linux-arm64 --self-contained -r linux-arm64
+dotnet publish -c Release -o $directory/publish/pboviewer-gui-windows-x64 --self-contained -r win-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-gui-windows-arm64 --self-contained -r win-arm64
+dotnet publish -c Release -o $directory/publish/pboviewer-gui-macos-x64 --self-contained -r osx-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-gui-macos-arm64 --self-contained -r osx-arm64
+dotnet publish -c Release -o $directory/publish/pboviewer-gui-linux-x64 --self-contained -r linux-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-gui-linux-arm64 --self-contained -r linux-arm64
