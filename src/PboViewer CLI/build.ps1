1201ca5f32235d58a95209904cc248627d8aba0f
Thomas Croizet <t.croizet77@gmail.com>
2025-01-02 12:55:27 +0100

    Modify build files

--- src/PboViewer CLI/build.ps1
+++ src/PboViewer CLI/build.ps1
@@ -1,4 +1,7 @@
 $directory = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
-dotnet publish -c Release -o $directory/publish/windows --self-contained -r win-x64
-dotnet publish -c Release -o $directory/publish/macos --self-contained -r osx-x64
-dotnet publish -c Release -o $directory/publish/linux --self-contained -r linux-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-windows-x64 --self-contained -r win-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-windows-arm64 --self-contained -r win-arm64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-macos-x64 --self-contained -r osx-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-macos-arm64 --self-contained -r osx-arm64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-linux-x64 --self-contained -r linux-x64
+dotnet publish -c Release -o $directory/publish/pboviewer-cli-linux-arm64 --self-contained -r linux-arm64
